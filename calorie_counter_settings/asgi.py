baee6edfbe3d16a3af7a47ec9400d0eb4289ff3b
Arkasha <doggypopycock@gmail.com>
2025-11-15 18:22:03 +0300

    feat: Completed initial project base. Registered apps and applied migrations.

--- calorie_counter_settings/asgi.py
+++ calorie_counter_settings/asgi.py
@@ -1,1 +1,16 @@
+"""
+ASGI config for calorie_counter_settings project.
 
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calorie_counter_settings.settings')
+
+application = get_asgi_application()
