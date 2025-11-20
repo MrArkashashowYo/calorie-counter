8476b42c25c53c2255876cc4890023021888dff2
artemka-2006 <artemsokol572@gmail.com>
2025-11-20 17:24:21 +0300

    feat:убрал плотную

--- meals/models.py
+++ meals/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True)
     calories = models.IntegerField()
 
     def __str__(self):
         return self.name
+git commit -m "feat:убрал плотную"
